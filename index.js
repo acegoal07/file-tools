0dd05f9cbebad3d4147204fc630368b9326a3cbd
acegoal07 <acegoal07@gmail.com>
2022-10-04 21:28:13 +0100

    1.0.0

--- index.js
+++ index.js
@@ -5,6 +5,38 @@ const rJson = require("r-json"),
 ///////////////////////////////////////////////////////////////////////////
 // General Tools //////////////////////////////////////////////////////////
 /**
+ * Creates a file at the location you provided 
+ * 
+ * @param {String} path The path to the file you want to create
+ * @param {Anything} data The data you want to put in the file
+ */
+exports.createFile = function(path, data = "") {
+   if (!path) {
+      throw new Error("ERROR with createFile: Path is null");
+   }
+   fs.writeFile(path, data, function (error) {
+      if (error) throw error;
+   });
+}
+
+/**
+ * Deletes the file you specify
+ * 
+ * @param {String} path The path to the file you want to delete
+ */
+exports.deleteFile = function(path) {
+   if (!path) {
+      throw new Error("ERROR with deleteFile: path is null");
+   }
+   if (!exports.fileExists(path)) {
+      throw new Error(`ERROR with deleteFile: File ${path} does not exists`);
+   }
+   fs.unlink(path, function (error) {
+      if (error) throw error;
+   });
+}
+
+/**
  * Returns a boolean whether or not the file exists
  * 
  * @param {String} path The path to the file you want to check
@@ -91,4 +123,61 @@ exports.getJsonFiles = function(dir) {
       }
    }
    return array;
+}
+
+/**
+ * Returns the data from the specified file
+ * 
+ * @param {String} path The path to the file you want to read
+ * @returns {Object} The data from the file
+ */
+exports.readJsonFile = function(path) {
+   if (!path) {
+      throw new Error("ERROR with readFile: path is null");
+   }
+   if (!exports.fileExists(path)) {
+      throw new Error(`ERROR with readFile: The provided file does not exists`);
+   }
+   return rJson(path);
+}
+
+/**
+ * Returns the data from all the json files in a folder either as a map or array
+ * 
+ * @param {String} dir The path to the directory you want to read data from
+ * @param {"Map" | "Array"} format The format you want to the returned 
+ * @returns {Map | Array} The data returned
+ */
+exports.readAllJsonFiles = function(dir, format = "Map") {
+   if (!dir) {
+      throw new Error("ERROR with readAllFiles: dir is null");
+   }
+   if (!exports.dirExists(dir)) {
+      throw new Error(`ERROR with readAllFiles: The provided folder does not exists`);
+   }
+   if (format === "Array") {
+      let array = new Array();
+      for (const file of fs.readdirSync(dir)) {
+         if (file.toLowerCase().endsWith(".json")) {
+            array.push(
+               {
+                  file: file.toLowerCase().replace(".json", ""),
+                  data: rJson(`${dir}/${file}`)
+               }
+            );
+         }
+         void(0);
+      }
+      return array;
+   } else {
+      const map = new Map();
+      for (const file of fs.readdirSync(dir)) {
+         if (file.toLowerCase().endsWith(".json")) {
+            map.set(file.toLowerCase().replace(".json", ""), rJson(`${dir}/${file}`));
+            
+         }
+         void(0);
+      }
+      return map;
+   }
 }
