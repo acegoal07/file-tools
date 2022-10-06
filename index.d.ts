2cf2dded42f59e61ddc187866084d42368482f3d
acegoal07 <acegoal07@gmail.com>
2022-10-06 15:42:09 +0100

    1.0.0

---
+++ index.d.ts
@@ -0,0 +1,83 @@
+export function universalFileTools(): UniversalFileTools;
+export function JsonFileTools(): JsonFileTools;
+/**
+ * The universal tools class
+ */
+declare class UniversalFileTools {
+    /**
+     * Creates a file at the location you provided
+     *
+     * @param {String} path The path to the file you want to create
+     * @param {Anything} data The data you want to put in the file
+     */
+    createFile(path: string, content?: string): void;
+    /**
+     * Deletes the file you specify
+     *
+     * @param {String} path The path to the file you want to delete
+     */
+    deleteFile(path: string): void;
+    /**
+     * Moves the file from the old location to the new location
+     *
+     * @param {String} path The path to the file you want to move
+     * @param {String} newDir The path to the folder you want to move the file to
+     */
+    moveFile(path: string, newDir: string): void;
+    /**
+     * Duplicates the file you specify
+     *
+     * @param {String} path The path to the file you want to copy
+     * @param {String} copyPath The path to the location you want the new file saved
+     */
+    copyFile(path: string, copyPath: string): void;
+    /**
+     * Renames the specified file to the new provided name
+     *
+     * @param {String} path The path to the file you want to rename
+     * @param {String} newName The name you want to change the file to
+     */
+    renameFile(path: string, newName: string): void;
+    /**
+     * Returns a boolean whether or not the file exists
+     *
+     * @param {String} path The path to the file you want to check
+     * @returns {Boolean} A boolean
+     */
+    fileExists(path: string): boolean;
+    /**
+     * Returns a boolean whether or not the directory exists
+     *
+     * @param {String} dir The path to the directory you want to check
+     * @returns {Boolean} A boolean
+     */
+    dirExists(dir: string): boolean;
+}
+/**
+ * The json tools class
+ */
+declare class JsonFileTools {
+    /**
+     * Returns the data from the specified file
+     *
+     * @param {String} path The path to the file you want to read
+     * @returns {Object} The data from the file
+     */
+    readFile(path: string): any;
+    /**
+     * Returns the data from all the json files in a folder either as a map or array
+     *
+     * @param {String} dir The path to the directory you want to read data from
+     * @param {"Map" | "Array"} format The format you want to the returned
+     * @returns {Map | Array} The data returned
+     */
+    readAllFiles(dir: string, format?: "Map" | "Array"): Map | any[];
+    /**
+     * Returns an array of the files in the directory that are json files
+     *
+     * @param {String} dir The path to the directory
+     * @returns {Array} An array of names
+     */
+    getFiles(dir: string): any[];
+}
+export {};
--- index.js
+++ index.js
@@ -5,179 +5,245 @@ const rJson = require("r-json"),
 ///////////////////////////////////////////////////////////////////////////
 // General Tools //////////////////////////////////////////////////////////
 /**
- * Creates a file at the location you provided 
+ * Returns a class filled with tools that can be used on any file type
  * 
- * @param {String} path The path to the file you want to create
- * @param {Anything} data The data you want to put in the file
+ * @returns {UniversalFileTools} An instance of the class
  */
-exports.createFile = function(path, data = "") {
-   if (!path) {
-      throw new Error("ERROR with createFile: Path is null");
-   }
-   fs.writeFile(path, data, function (error) {
-      if (error) throw error;
-   });
+ exports.universalFileTools = function() {
+   return new UniversalFileTools();
 }
 
 /**
- * Deletes the file you specify
- * 
- * @param {String} path The path to the file you want to delete
+ * The universal tools class
  */
-exports.deleteFile = function(path) {
-   if (!path) {
-      throw new Error("ERROR with deleteFile: path is null");
-   }
-   if (!exports.fileExists(path)) {
-      throw new Error(`ERROR with deleteFile: File ${path} does not exists`);
-   }
-   fs.unlink(path, function (error) {
-      if (error) throw error;
-   });
-}
+class UniversalFileTools {
 
-/**
- * Returns a boolean whether or not the file exists
- * 
- * @param {String} path The path to the file you want to check
- * @returns {Boolean} A boolean
- */
-exports.fileExists = function(path) {
-   if (!path) {
-      throw new Error("ERROR with fileExist: path is null");
-   }
-   if (!fs.statSync(path).isFile()) {
-      throw new Error("ERROR with fileExist: The path you have provided is not to a file");
-   }
-   return fs.existsSync(path);
-}
-
-/**
- * Returns a boolean whether or not the directory exists
- * 
- * @param {String} dir The path to the directory you want to check
- * @returns {Boolean} A boolean
- */
-exports.dirExists = function(dir) {
-   if (!dir) {
-      throw new Error("ERROR with dirExist: path is null");
-   }
-   if (!fs.statSync(dir).isDirectory()) {
-      throw new Error("ERROR with dirExist: The path you have provided is not to a directory");
-   }
-   return fs.existsSync(dir);
-}
-///////////////////////////////////////////////////////////////////////////
-// Json Tools /////////////////////////////////////////////////////////////
-class JsonTools {
-   constructor(path) {
-      this.path = path;
+   /**
+    * Creates a file at the location you provided 
+    * 
+    * @param {String} path The path to the file you want to create
+    * @param {Anything} data The data you want to put in the file
+    */
+   createFile(path, content = "") {
+      if (!path) {
+         throw new Error("ERROR with createFile: Path is null");
+      }
+      fs.writeFile(path, content, function (error) {
+         if (error) throw error;
+      });      
    }
 
    /**
-    * Reads all the data from the file
+    * Deletes the file you specify
     * 
-    * @returns {Object} The data to the file
+    * @param {String} path The path to the file you want to delete
     */
-   read() {
-      return rJson(this.path);
-   }   
-}
+   deleteFile(path) {
+      if (!path) {
+         throw new Error("ERROR with deleteFile: path is null");
+      }
+      if (!this.fileExists(path)) {
+         throw new Error(`ERROR with deleteFile: File ${path} does not exists`);
+      }
+      fs.unlink(path, function (error) {
+         if (error) throw error;
+      });
+   }  
 
-/**
- * Takes in the path to the json file and provides tools for it
- * 
- * @param {String} path The path to the json file or directory
- * @returns {JsonTools} An instance of the json tools
- */
-exports.jsonFile = function(path) {
-   if (!path) {
-      throw new Error("ERROR with jsonFile: path is null");
+   /**
+    * Moves the file from the old location to the new location
+    * 
+    * @param {String} path The path to the file you want to move
+    * @param {String} newDir The path to the folder you want to move the file to
+    */
+   moveFile(path, newDir) {
+      if (!path) {
+         throw new Error("ERROR with moveFile: oldPath is null");
+      }
+      if (!this.fileExists(path)) {
+         throw new Error(`ERROR with moveFile: File ${path} does not exists`);
+      }
+      if (!newDir) {
+         throw new Error("ERROR with moveFile: newPath is null");
+      }
+      fs.copyFile(path, newDir, function (error) {
+         if (error) throw error;
+      });
+      this.deleteFile(path);
    }
-   if (!exports.fileExists(path)) {
-      throw new Error("ERROR with jsonFile: There is no file at the path you have provided");
-   }
-   return new JsonTools(path);
-}
 
-/**
- * Returns an array of the files in the directory that are json files
- * 
- * @param {String} dir The path to the directory
- * @returns {Array} An array of names
- */
-exports.getJsonFiles = function(dir) {
-   if (!dir) {
-      throw new Error("ERROR with getJsonFiles: dir is null");
-   }
-   if (!fs.statSync(dir).isDirectory()) {
-      throw new Error("ERROR with getJsonFile: The path you have provided is not to a file");
-   }
-   if (!exports.dirExists(dir)) {
-      throw new Error("ERROR with getJsonFile: There is no dir at the path you have provided");
-   }
-   let array = [];
-   for (const file of fs.readdirSync(dir)) {
-      if (file.toLowerCase().endsWith(".json")) {
-         array.push(file);
+   /**
+    * Duplicates the file you specify
+    * 
+    * @param {String} path The path to the file you want to copy
+    * @param {String} copyPath The path to the location you want the new file saved
+    */
+   copyFile(path, copyPath) {
+      if (!path) {
+         throw new Error("ERROR with copyFile: path is null");
+      }
+      if (!this.fileExists(path)) {
+         throw new Error(`ERROR with copyFile: File ${path} does not exists`);
+      }
+      if (!copyPath) {
+         fs.copyFile(path, path.replace(".json","-copy.json"), function (error) {
+            if (error) throw error;
+         });
+      } else {
+         fs.copyFile(path, copyPath, function (error) {
+            if (error) throw error;
+         });
       }
    }
-   return array;
+
+   /**
+    * Renames the specified file to the new provided name
+    * 
+    * @param {String} path The path to the file you want to rename
+    * @param {String} newName The name you want to change the file to
+    */
+   renameFile(path, newName) {
+      if (!path) {
+         throw new Error("ERROR with renameFile: path is null");
+      }
+      if (!this.fileExists(path)) {
+         throw new Error(`ERROR with renameFile: The provided file does not exists`);
+      }
+      if (!newName) {
+         throw new Error("ERROR with renameFile: newName is null")
+      }
+      fs.rename(path, path.replace(path.split(/[\\/]/).pop(), newName), function (error) {
+         if (error) throw error;
+      });
+   }
+
+   /**
+    * Returns a boolean whether or not the file exists
+    * 
+    * @param {String} path The path to the file you want to check
+    * @returns {Boolean} A boolean
+    */
+   fileExists(path) {
+      if (!path) {
+         throw new Error("ERROR with fileExist: path is null");
+      }
+      if (!fs.statSync(path).isFile()) {
+         throw new Error("ERROR with fileExist: The path you have provided is not to a file");
+      }
+      return fs.existsSync(path);
+   }
+
+   /**
+    * Returns a boolean whether or not the directory exists
+    * 
+    * @param {String} dir The path to the directory you want to check
+    * @returns {Boolean} A boolean
+    */
+   dirExists(dir) {
+      if (!dir) {
+         throw new Error("ERROR with dirExist: path is null");
+      }
+      if (!fs.statSync(dir).isDirectory()) {
+         throw new Error("ERROR with dirExist: The path you have provided is not to a directory");
+      }
+      return fs.existsSync(dir);
+   }
+}
+///////////////////////////////////////////////////////////////////////////
+// Json Tools /////////////////////////////////////////////////////////////
+/**
+ * Returns a class filling with tools specific for json files
+ * 
+ * @returns {JsonFileTools} An instance of the class
+ */
+exports.JsonFileTools = function() {
+   return new JsonFileTools();
 }
 
 /**
- * Returns the data from the specified file
- * 
- * @param {String} path The path to the file you want to read
- * @returns {Object} The data from the file
+ * The json tools class
  */
-exports.readJsonFile = function(path) {
-   if (!path) {
-      throw new Error("ERROR with readFile: path is null");
-   }
-   if (!exports.fileExists(path)) {
-      throw new Error(`ERROR with readFile: The provided file does not exists`);
-   }
-   return rJson(path);
-}
+class JsonFileTools {
 
-/**
- * Returns the data from all the json files in a folder either as a map or array
- * 
- * @param {String} dir The path to the directory you want to read data from
- * @param {"Map" | "Array"} format The format you want to the returned 
- * @returns {Map | Array} The data returned
- */
-exports.readAllJsonFiles = function(dir, format = "Map") {
-   if (!dir) {
-      throw new Error("ERROR with readAllFiles: dir is null");
+   /**
+    * Returns the data from the specified file
+    * 
+    * @param {String} path The path to the file you want to read
+    * @returns {Object} The data from the file
+    */
+   readFile(path) {
+      if (!path) {
+         throw new Error("ERROR with readFile: path is null");
+      }
+      if (!exports.universalFileTools().fileExists(path)) {
+         throw new Error(`ERROR with readFile: The provided file does not exists`);
+      }
+      return rJson(path);
    }
-   if (!exports.dirExists(dir)) {
-      throw new Error(`ERROR with readAllFiles: The provided folder does not exists`);
+
+   /**
+    * Returns the data from all the json files in a folder either as a map or array
+    * 
+    * @param {String} dir The path to the directory you want to read data from
+    * @param {"Map" | "Array"} format The format you want to the returned 
+    * @returns {Map | Array} The data returned
+    */
+   readAllFiles(dir, format = "Map") {
+      if (!dir) {
+         throw new Error("ERROR with readAllFiles: dir is null");
+      }
+      if (!exports.universalFileTools().dirExists(dir)) {
+         throw new Error(`ERROR with readAllFiles: The provided folder does not exists`);
+      }
+      if (format === "Array") {
+         let array = new Array();
+         for (const file of fs.readdirSync(dir)) {
+            if (file.toLowerCase().endsWith(".json")) {
+               array.push(
+                  {
+                     file: file.toLowerCase().replace(".json", ""),
+                     data: rJson(`${dir}/${file}`)
+                  }
+               );
+            }
+            void(0);
+         }
+         return array;
+      } else {
+         const map = new Map();
+         for (const file of fs.readdirSync(dir)) {
+            if (file.toLowerCase().endsWith(".json")) {
+               map.set(file.toLowerCase().replace(".json", ""), rJson(`${dir}/${file}`));
+               
+            }
+            void(0);
+         }
+         return map;
+      }
    }
-   if (format === "Array") {
-      let array = new Array();
+
+   /**
+    * Returns an array of the files in the directory that are json files
+    * 
+    * @param {String} dir The path to the directory
+    * @returns {Array} An array of names
+    */
+   getFiles(dir) {
+      if (!dir) {
+         throw new Error("ERROR with getJsonFiles: dir is null");
+      }
+      if (!fs.statSync(dir).isDirectory()) {
+         throw new Error("ERROR with getJsonFile: The path you have provided is not to a file");
+      }
+      if (!exports.universalFileTools().dirExists(dir)) {
+         throw new Error("ERROR with getJsonFile: There is no dir at the path you have provided");
+      }
+      let array = [];
       for (const file of fs.readdirSync(dir)) {
          if (file.toLowerCase().endsWith(".json")) {
-            array.push(
-               {
-                  file: file.toLowerCase().replace(".json", ""),
-                  data: rJson(`${dir}/${file}`)
-               }
-            );
+            array.push(file);
          }
-         void(0);
       }
       return array;
-   } else {
-      const map = new Map();
-      for (const file of fs.readdirSync(dir)) {
-         if (file.toLowerCase().endsWith(".json")) {
-            map.set(file.toLowerCase().replace(".json", ""), rJson(`${dir}/${file}`));
-            
-         }
-         void(0);
-      }
-      return map;
    }
 }
