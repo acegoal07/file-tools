d4e271407ffe30fb2094b6aa0f7a77282fca7691
acegoal07 <acegoal07@gmail.com>
2023-01-14 14:31:48 +0000

    1.0.3

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export function UniversalFileTools(): UniversalFileTools;
-export function JsonFileTools(): JsonFileTools;
-import { UniversalFileTools } from "./lib/UniversalFileTools";
-import { JsonFileTools } from "./lib/JsonFileTools";
