f37b9f21180f653550d1d6cfc261ed4562ab4aec
acegoal07 <39467245+acegoal07@users.noreply.github.com>
2023-01-19 21:28:21 +0000

    1.0.4

--- test.js
+++ test.js
@@ -1,15 +1,34 @@
 const fileTools = require("./dist");
+const top_bottom = "|------------------------------|--------|"
+
+/**
+ *
+ * @param {String} testName
+ * @param {Boolean} value
+ */
+function logFormatter (testName, value = false) {
+    try {
+        const size = 28;
+        const output = `| ${testName}${" ".repeat((size - testName.length))} | ${value? "\x1b[92mPASSED\x1b[0m" : "\x1b[91mFAILED\x1b[0m"} |`
+        console.log(output);
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+console.log(top_bottom);
+console.log("|             TEST             | STATUS |")
+console.log(top_bottom);
 
 // Create directory
 try {
     fileTools.UniversalFileTools().createDir("testFiles");
     if (fileTools.UniversalFileTools().dirExists("testFiles")) {
-        console.log("\x1b[92mDirectory creation test passed\x1b[0m");
+        logFormatter('Directory creation', true);
     } else {
-        console.log("\x1b[91mDirectory creation test Failed\x1b[0m");
+        logFormatter('Directory creation');
     }
 } catch (error) {
-    console.log("\x1b[91mDirectory creation test Failed\x1b[0m");
+    logFormatter('Directory creation');
     throw new Error(error);
 }
 
@@ -17,12 +36,18 @@ try {
 try {
     fileTools.UniversalFileTools().createFile("testFiles/test.txt");
     if (fileTools.UniversalFileTools().fileExists("testFiles/test.txt")) {
-        console.log("\x1b[92mFile creation test passed\x1b[0m");
+        logFormatter('File creation test 1', true);
     } else {
-        console.log("\x1b[91mFile creation test Failed\x1b[0m");
+        logFormatter('File creation test 1');
+    }
+    fileTools.UniversalFileTools().createFile("testFiles/testTxt.txt", "hello");
+    if (fileTools.UniversalFileTools().fileExists("testFiles/testTxt.txt") && fileTools.UniversalFileTools().readFile("testFiles/testTxt.txt") === "hello") {
+        logFormatter('File creation test 2', true);
+    } else {
+        logFormatter('File creation test 2');
     }
 } catch (error) {
-    console.log("\x1b[91mFile creation test Failed\x1b[0m");
+    logFormatter('File creation');
     throw new Error(error);
 }
 
@@ -30,12 +55,12 @@ try {
 try {
     fileTools.UniversalFileTools().copyFile("testFiles/test.txt");
     if (fileTools.UniversalFileTools().fileExists("testFiles/test-copy.txt")) {
-        console.log("\x1b[92mFile copy test passed\x1b[0m");
+        logFormatter('File copy', true);
     } else {
-        console.log("\x1b[91mFile copy test Failed\x1b[0m");
+        logFormatter('File copy');
     }
 } catch (error) {
-    console.log("\x1b[91mFile copy test Failed\x1b[0m");
+    logFormatter('File copy');
     throw new Error(error);
 }
 
@@ -43,51 +68,104 @@ try {
 try {
     fileTools.UniversalFileTools().deleteFile("testFiles/test-copy.txt");
     if (!fileTools.UniversalFileTools().fileExists("testFiles/test-copy.txt")) {
-        console.log("\x1b[92mFile deletion test passed\x1b[0m");
+        logFormatter('File deletion', true);
     } else {
-        console.log("\x1b[91mFile deletion test Failed\x1b[0m");
+        logFormatter('File deletion');
     }
 } catch (error) {
-    console.log("\x1b[91mFile deletion test Failed\x1b[0m");
+    logFormatter('File deletion');
     throw new Error(error);
 }
 
 // Is directory empty
 try {
     if (!fileTools.UniversalFileTools().isDirEmpty("testFiles")) {
-        console.log("\x1b[92mIs directory empty test passed\x1b[0m");
+        logFormatter('Is directory empty', true);
     } else {
-        console.log("\x1b[91mIs directory empty test Failed\x1b[0m");
+        logFormatter('Is directory empty');
     }
 } catch (error) {
-    console.log("\x1b[91mIs directory empty test Failed\x1b[0m");
+    logFormatter('Is directory empty');
     throw new Error(error);
 }
 
 // Rename file
 try {
     fileTools.UniversalFileTools().renameFile("testFiles/test.txt", "test2.txt");
-    if (fileTools.UniversalFileTools().fileExists("testFiles/test2")) {
-        console.log("\x1b[92mRename file test test passed\x1b[0m");
+    if (fileTools.UniversalFileTools().fileExists("testFiles/test2.txt")) {
+        logFormatter('Rename file', true);
     } else {
-        console.log("\x1b[91mRename file test test Failed\x1b[0m");
+        logFormatter('Rename file');
     }
 } catch (error) {
-    console.log("\x1b[91mRename file test Failed\x1b[0m");
+    logFormatter('Rename file');
     throw new Error(error);
 }
 
-return;
+// Move file
+try {
+    fileTools.UniversalFileTools().moveFile("testFiles/test2.txt", "test2.txt");
+    if (fileTools.UniversalFileTools().fileExists("test2.txt")) {
+        logFormatter('Move file', true);
+    } else {
+        logFormatter('Move file');
+    }
+    fileTools.UniversalFileTools().moveFile("test2.txt", "testFiles/test.txt");
+} catch (error) {
+    logFormatter('Move file');
+    throw new Error(error);
+}
+
+// Write file
+try {
+    fileTools.UniversalFileTools().writeFile("testFiles/testTxt.txt", "passed");
+    if (fileTools.UniversalFileTools().readFile("testFiles/testTxt.txt") === "passed") {
+        logFormatter('Write file', true);
+    } else {
+        logFormatter('Write file');
+    }
+} catch (error) {
+    logFormatter('Write file');
+    throw new Error(error);
+}
+
+// Write copy file
+try {
+    fileTools.UniversalFileTools().writeCopy("testFiles/testTxt.txt", "testFiles/test.txt");
+    if (fileTools.UniversalFileTools().readFile("testFiles/test.txt") === "passed") {
+        logFormatter('Write copy', true);
+    } else {
+        logFormatter('Write copy');
+    }
+} catch (error) {
+    logFormatter('Write copy');
+    throw new Error(error);
+}
+
+// Get file type
+try {
+    if (fileTools.UniversalFileTools().getFileType("testFiles", "txt").length == 2) {
+        logFormatter('Get file type', true);
+    } else {
+        logFormatter('Get file type');
+    }
+} catch (error) {
+    console.log("hello")
+    logFormatter('Get file type');
+    throw new Error(error);
+}
 
 // Delete directory
 try {
     fileTools.UniversalFileTools().deleteDir("testFiles", true);
     if (!fileTools.UniversalFileTools().dirExists("testFiles")) {
-        console.log("\x1b[92mDirectory deletion test passed\x1b[0m");
+        logFormatter('Directory deletion', true);
     } else {
-        console.log("\x1b[91mDirectory deletion test Failed\x1b[0m");
+        logFormatter('Directory deletion');
     }
 } catch (error) {
-    console.log("\x1b[91mDirectory deletion test Failed\x1b[0m");
+    logFormatter('Directory deletion');
     throw new Error(error);
 }
+
+console.log(top_bottom);
